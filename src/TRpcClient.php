ac5da4ecab924db532e5868d16082fc26c04822b
YCJ <yuchangjiang@hulianpai.com>
2022-10-17 11:07:48 +0800

    修改

--- src/TRpcClient.php
+++ src/TRpcClient.php
@@ -1,4 +1,5 @@
 <?php
+/** @noinspection ALL */
 declare(strict_types=1);
 
 namespace Hyperf\ThriftRpc;
@@ -6,30 +7,36 @@ namespace Hyperf\ThriftRpc;
 class TRpcClient
 {
     private static $instance;
+    private static $_async = false;
 
     static function getInstance(...$args)
     {
+        self::$_async = false;
         if (!isset(self::$instance)) {
-            self::$instance = new static(...$args);
+//            self::$instance = new static(...$args);
+            self::$instance = make(static::class, $args);
         }
         return self::$instance;
     }
 
     private $client;
 
+
     public function __construct($name)
     {
         $this->client = make(TRpcClientResolve::class)->get($name);
     }
 
-    public function async()
+    public static function async($name)
     {
-        return $this->client->_async(true);
+        $instance = self::getInstance($name);
+        self::$_async = true;
+        return $instance;
     }
 
     public function __call($name, $arguments)
     {
-        return $this->client->_async(false)->{$name}(...$arguments);
+        return $this->client->_async(self::_async)->{$name}(...$arguments);
     }
 
 }
