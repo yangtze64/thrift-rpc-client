f49075e789bd5c3b7302f8e65551fb4345e7a9f6
Yangtze <594998624@qq.com>
2022-10-26 14:41:53 +0800

    修改rpc客户端文件

--- src/TRpcClient.php
+++ src/TRpcClient.php
@@ -6,37 +6,20 @@ namespace Hyperf\ThriftRpc;
 
 class TRpcClient
 {
-    private static $instance;
-    private static $_async = false;
-
-    static function getInstance(...$args)
-    {
-        self::$_async = false;
-        if (!isset(self::$instance)) {
-//            self::$instance = new static(...$args);
-            self::$instance = make(static::class, $args);
-        }
-        return self::$instance;
-    }
-
     private $client;
-
 
     public function __construct($name)
     {
         $this->client = make(TRpcClientResolve::class)->get($name);
     }
 
-    public static function async($name)
-    {
-        $instance = self::getInstance($name);
-        self::$_async = true;
-        return $instance;
-    }
-
     public function __call($name, $arguments)
     {
-        return $this->client->_async(self::$_async)->{$name}(...$arguments);
+        $async = false;
+        if (0 === strpos($name, 'async')) {
+            $async = true;
+            $name = substr($name, 5);
+        }
+        return $this->client->_async($async)->{$name}(...$arguments);
     }
-
 }
